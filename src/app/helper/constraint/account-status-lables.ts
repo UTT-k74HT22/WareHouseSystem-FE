684fdfadb743355ada8be7ce8fe7535fe4136ed7
DungDev <hoangdinhdung0205@gmail.com>
2026-02-04 22:08:31 +0700

    feat: fix warehouse + account info (manager)

---
+++ src/app/helper/constraint/account-status-lables.ts
@@ -0,0 +1,8 @@
+import {AccountStatus} from "../enums/AccountStatus";
+
+export const ACCOUNT_STATUS_LABELS: Record<AccountStatus, string> = {
+  [AccountStatus.ACTIVE]: 'Hoạt động',
+  [AccountStatus.INACTIVE]: 'Không hoạt động',
+  [AccountStatus.SUSPENDED]: 'Tạm ngưng',
+  [AccountStatus.DELETE]: 'Đã xóa'
+}
---
+++ src/app/service/Account/account.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {BaseURL} from "../../../environments/BaseURL";
+import {Observable} from "rxjs";
+import {ApiResponse} from "../../dto/response/ApiResponse";
+import {AccountResponse} from "../../dto/response/ Account/AccountResponse";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  private readonly apiUrl = `${BaseURL.API_URL}users`;
+
+  constructor(private http: HttpClient) { }
+
+  getUserByRoleManager(): Observable<ApiResponse<AccountResponse[]>> {
+    return this.http.get<ApiResponse<AccountResponse[]>>(`${this.apiUrl}/managers`);
+  }
+}
