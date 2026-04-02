208920bece483e2afb41d1b35a04897d2d4f47f8
hoandevv <dohoan25112005@gmail.com>
2026-04-02 18:59:21 +0700

    Fix UIUX

--- src/app/dto/response/Inventory/InventorySummaryResponse.ts
+++ src/app/dto/response/Inventory/InventorySummaryResponse.ts
@@ -3,6 +3,7 @@ export interface InventorySummaryResponse {
   product_sku: string | null;
   product_name: string | null;
   total_on_hand_quantity: number;
+  total_quarantine_quantity?: number;
   total_reserved_quantity: number;
   total_available_quantity: number;
   warehouse_count: number;
